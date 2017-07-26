10083f82fa54fd7b6c1311d7e3fa404b27e2162f
zhaoyanqi <xiaomogu@xiaomogudeMac-mini.local>
2017-07-26 12:19:56 +0800

    project init

---
+++ CarthageDemo/CarthageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CarthageDemo
+//
+//  Created by zhaoyanqi on 2017/7/26.
+//  Copyright © 2017年 yanqizhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CarthageDemo/CarthageDemoTests/CarthageDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  CarthageDemoTests.m
+//  CarthageDemoTests
+//
+//  Created by zhaoyanqi on 2017/7/26.
+//  Copyright © 2017年 yanqizhao. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CarthageDemoTests : XCTestCase
+
+@end
+
+@implementation CarthageDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
